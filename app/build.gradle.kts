5c3b220310ec92c5b39716bd140531aa23ae2fe8
joeripronk <git@joeri.nu>
2026-09-22 15:59:43 +0200

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.swimgym.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
