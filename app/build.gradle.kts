552157f1feb2e0012cbd3a7310bcbb904c7e95b4
joeripronk <git@joeri.nu>
2026-09-23 15:14:01 +0200

    release swimgym v1.0p27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.swimgym.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 26
+        versionCode = 27
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
